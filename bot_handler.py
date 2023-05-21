1cf3b1794e6a021278ca58449b3c43531e16f3cc
Nikitonz <100299461+Nikitonz@users.noreply.github.com>
2023-05-21 23:29:22 +0300

    some additions

--- bot_handler.py
+++ bot_handler.py
@@ -1,41 +1,107 @@
-import discord
+import argparse
+import asyncio
+from datetime import datetime, date, timedelta
+
+import disnake
+from disnake.ui import Select
+from disnake.ext import commands
+
+import requests
+import json
 from discord.ext import commands
+from disnake import ApplicationCommandInteraction, SelectOption
 from mcstatus import JavaServer
 
-import main
+config = []
+params = [[]]
 
-bot_token = ""
-channel_ids = []
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), test_guilds=channel_ids)
-server = JavaServer.lookup(f"{main.getIP()}")
+def load(fileP):
+    with open(fileP, 'r') as file:
+        data = json.load(file)
+    config = data.get('config', [])
+    params = data.get('params', [])
+    return (config, params)
+
+
+def add_data(self, param1: str, param2: str, param3: str, param4: int):
+    self.params.append((param1, param2, param3, param4))
+
+
+def getIP():
+    response = requests.get("https://httpbin.org/ip")
+    data = response.json()
+    ip = data["origin"]
+    return ip
+
+
+bot = disnake.ext.commands.Bot(command_prefix="!", intents=disnake.Intents.all())
+
 
 @bot.event
 async def on_ready():
-    pass
+    print(f"bot {bot.user} started")
 
 
 @bot.event
-async def on_message(message):
-    await bot.process_commands(message)
+async def on_disconnect():
+    print(f"bot {bot.user} откис")
 
 
-@bot.command(name="online", aliases=["get", "o"])
-async def online(ctx):
+# ---------------------------------------
+@bot.slash_command(name='managegame', help="allows you to change add custom game to spectrate",
+                   description="allows you to change add custom game to spectrate")
+async def managegm(inter, gamename: str, icon_url: str, ip: str, port: int):
+    params.append((gamename, icon_url, ip, port))
+    await inter.response.edit_message(content=f"Игра {gamename} добавлена в список", view=None)
+
+
+# ---------------------------------------
+@bot.slash_command(name='online', help="1", description="displays some infos")
+async def online(inter):
+
     try:
-        print("number online called")
-        status = server.status()
-        message = f"На сервере сейчас играют {status.players.online} игроков."
-    except:
-        message = "Сервер не отвечает."
+        selected_option = None
+        options_list = [param['name'] for param in params]
+        options = [SelectOption(label=option, value=option, default=0) for option in options_list]
+        select = Select(options=options, placeholder="Игра", custom_id="gamePicker")
 
-    await ctx.send(message)
+        await inter.response.send_message("Выберите игру:", components=[select], ephemeral=True, delete_after=40)
+
+        @bot.event
+        async def on_message_interaction(interaction):
+            nonlocal selected_option
+            if interaction.data.custom_id == 'gamePicker':
+                try:
+                    print("passed #1")
+                    selected_option = interaction.data.values[0]
+                    #await interaction.response.edit_message(content=selected_option, view=None)
+                except Exception as error:
+                    await interaction.send(str(error), ephemeral=True)
+
+        print("passed #2")
+        await bot.wait_for("message_interaction", check=lambda i: i.component.custom_id == 'gamePicker')
+        print("passed #3")
+        print(f"{selected_option}")
+        for param in params:
+            if param['name'] == selected_option:
+                server = JavaServer.lookup(f"{param['ip']}:" + str(param['port']))
+                status = server.status()
+                message = f"На сервере {param['name']} сейчас играют {status.players.online} игроков."
+                await inter.send(str(message), ephemeral=True)
+                print("passed #4")
+                break
+        print("passed #final")
+    except asyncio.TimeoutError as er:
+        await inter.send("Превышено время ожидания выбора игры." + str(er), ephemeral=True)
+    except Exception as error:
+        await inter.send(str(error), ephemeral=True)
 
 
-@bot.command(name="who_online", aliases = ["getplrlist", "who"])
+@bot.command(name="who_online", aliases=["getplrlist", "who"])
 async def whoonline(ctx):
     try:
-
+        server = JavaServer.lookup(f"{params[0]['ip'] + params[0]['port']}")
         print("playerlist called")
         query = server.query()
         message = f"На сервере сейчас играют:\n{', '.join(query.players.names)}"
@@ -43,12 +109,56 @@ async def whoonline(ctx):
         message = "Сервер не отвечает."
     await ctx.send(message)
 
-@bot.command(name="ping")
-async def ping(ctx):
-    latency = server.ping()
-    try:
-        await ctx.send(f"Задержка составляет {latency} мс.")
-    except:
-        await ctx.send("Сервер не отвечает.")
 
-bot.run(bot_token)
+"""
+def start():
+    global time_launched
+    print("started")
+    time_launched = datetime.now().time()
+    ip = getIP()
+
+    data = {
+        "username": f"{gamename} Server alert",
+        "avatar_url": picture_url,
+        "embeds": [
+            {
+                "title": f"{gamename} server status",
+                "description": f"\nСервер запущен!\nIP: {ip}, Port: 25565\n```{ip}:25565```\nВремя запуска: {time_launched.strftime('%H:%M:%S')}",
+                "color": 0x00FF00
+            }
+        ]
+    }
+    for url in webhook_urls:
+        requests.post(url, json=data)
+    with open("time.txt", 'w') as inp:
+        inp.write(str(time_launched))
+
+
+def stop():
+    with open("time.txt", 'r') as gettime:
+        time_launched = datetime.strptime(gettime.readline(), '%H:%M:%S.%f').time()
+    print("finishing...")
+
+    time_stopped = datetime.now().time()
+    seconds_elapsed = (datetime.combine(date.today(), time_stopped) - datetime.combine(date.today(),
+                                                                                       time_launched)).total_seconds()
+
+    data = {
+        "username": f"{gamename} Server alert",
+        "avatar_url": picture_url,
+        "embeds": [
+            {
+                "title": f"{gamename} server status",
+                "description": f"\nСервер был остановлен. Время остановки: {time_stopped.strftime('%H:%M:%S')}\nСервер проработал={timedelta(seconds=seconds_elapsed)}",
+                "color": 0x8B0000
+            }
+        ]
+    }
+    for url in webhook_urls:
+        requests.post(url, json=data)
+
+"""
+if __name__ == '__main__':
+    (config, params) = load("config.json")
+
+bot.run(config[0]["token"])
--- start.bat
+++ start.bat
@@ -1,9 +1,98 @@
 chcp 65001
-cd "%~dp0"
-python3 main.py start
-start /MIN cmd /k python3 bot_handler.py
+set "sourceFolder=%CD%"
+set "targetFolder=%CD%\SAVES_bak"
+set "backupFolder=%CD%\SAVES_bak\SAVES_bak1"
+@echo off
+setlocal enabledelayedexpansion
+if not exist "%targetFolder%" (
+    mkdir "%targetFolder%"
+    echo Created targetFolder: %targetFolder%
+)
+@echo off
+if not exist "%backupFolder%" (
+    mkdir "%backupFolder%"
+    echo Created backupFolder: %backupFolder%
+)
+:optionality
+cls
+@echo Параметр запуска:
+@echo 1) сервер + уведомления + бот
+@echo 2) только сервер
+@echo 3) только бот
+@echo 4) создать бэкап миров
+@echo 5) создать бэкап бэкапа миров
+@echo 6) восстановить миры
+@echo 0) выход
+:optionality
+set /p option="Введите числовую опцию: "
+if "%option%"=="" set "option=1"
+
+
+if "%option%"=="1" goto option1
+if "%option%"=="2" goto option2
+if "%option%"=="3" goto option3
+if "%option%"=="4" goto option4
+if "%option%"=="5" goto option5
+if "%option%"=="6" goto option6
+if "%option%">"6" OR "%option%"<"1" goto end
+goto invalidOption
+
+:option1
+python3 bot_handler.py start
 java -Xmx1024M -Xms1024M -jar spigot-1.19.4.jar nogui
 python3 main.py stop
-pause
-exit
+goto end
 
+:option2
+java -Xmx1024M -Xms1024M -jar spigot-1.19.4.jar nogui
+goto end
+
+:option3
+Start PowerShell.exe -NoExit -Command "python bot_handler.py" 
+goto end
+
+:option4
+set /p option="Вы хотите перезаписать последний сейв? (y/n): "
+if "%option%"=="y" (
+if "%option%"=="n" goto end )
+else 
+rem Копирование папок world, world-nether, world_the_end в папку SAVES_bak
+xcopy /s /e /i /y "%sourceFolder%\world" "%targetFolder%\world"
+xcopy /s /e /i /y "%sourceFolder%\world_nether" "%targetFolder%\world-nether"
+xcopy /s /e /i /y "%sourceFolder%\world_the_end" "%targetFolder%\world_the_end"
+goto end
+
+:option5
+set /p option="Вы хотите создать резервную копию резервной копии? (y/n): "
+if "%option%"=="y" (
+  for /D %%G in ("%targetFolder%\*") do (
+    set "folderName=%%~nxG"
+    if not "%folderName%"=="SAVES_bak1" (
+        move /Y "%%G" "%backupFolder%\"
+    )
+  )
+pause
+) else (if "%option%"=="n" goto end )
+else (if not "%option%"=="y" or  not "%option%"=="n" goto option5)
+
+:option6
+set /p option="Восстановить резервную копию? ("newest_bak"/"oldest_bak"): "
+if "%option%"=="newest_bak" (
+xcopy /s /e /i /y "%targetFolder%\world" "%sourceFolder%\world"
+xcopy /s /e /i /y "%targetFolder%\world_nether" "%sourceFolder%\world-nether"
+xcopy /s /e /i /y "%targetFolder%\world_the_end" "%sourceFolder%\world_the_end"
+)
+else (if "%option%"=="oldest_bak" 
+(
+xcopy /s /e /i /y "%backupFolder%\world" "%sourceFolder%\world"
+xcopy /s /e /i /y "%backupFolder%\world_nether" "%sourceFolder%\world-nether"
+xcopy /s /e /i /y "%backupFolder%\world_the_end" "%sourceFolder%\world_the_end")
+)
+else goto optionality
+
+:invalidOption
+echo Некорректная опция
+goto optionality
+
+:end
+exit
